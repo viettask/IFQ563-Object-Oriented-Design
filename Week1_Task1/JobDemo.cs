9d181101f4058e409ab057105c74de27f5353f88
Viet Nguyen (Henry) <49942312+viettask@users.noreply.github.com>
2024-08-19 22:59:13 +1000

    Add files via upload

---
+++ Week1_Task1/JobDemo.cs
@@ -0,0 +1,98 @@
+﻿/*using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Step 1: Create an application named JobDemo that declares and uses Job objects. The Job class holds job information for a home repair service. The class has five properties that include a job number, customer name, job description, estimated hours, and price for the job.
+
+Step 2: Create a constructor that requires parameters for all the data except the price. Include auto-implemented properties for the job number, customer name, and job description, but not for hours or price; the price field value is calculated as estimated hours times $45.00 whenever the hours value is set.
+
+Step 3: Create the following for the class:
+
+An Equals() method that determines two Job s are equal if they have the same job number.
+ A GetHashCode() method that returns the job number.
+ A ToString() method that returns a string containing all job information.
+The JobDemo application declares a few Job objects, sets their values and demonstrates that all the methods work as expected.
+*/
+
+using System;
+using static System.Console;
+
+class JobDemo
+{
+    static void Main()
+    {
+        Job job1 = new Job(111, "Smith", "exterior paint", 20);
+        Job job2 = new Job(222, "Vega", "gutter clean", 4);
+        Job job3 = new Job(111, "Land", "blacktop drive", 10);
+        WriteLine(job1.ToString());
+        WriteLine(job2.ToString());
+        WriteLine(job3.ToString());
+        CompareNumbers(job1, job2);
+        CompareNumbers(job1, job3);
+    }
+    internal static void CompareNumbers(Job job1, Job job2)
+    {
+        if (job1.Equals(job2))
+            WriteLine("{0} for {1} has the same job " +
+            "number as {2} for {3}",
+            job1.JobNumber, job1.Customer,
+            job2.JobNumber, job2.Customer);
+    }
+}
+
+class Job
+{
+    private double hours;
+    private double price;
+    public const double RATE = 45.00;
+
+    //Constructor
+    public Job(int num, string cust, string desc, double hrs)
+    {
+        JobNumber = num;
+        Customer = cust;
+        Description = desc;
+        Hours = hrs;
+    }
+    public int JobNumber { get; set; }
+    public string Customer { get; set; }
+    public string Description { get; set; }
+    public double Hours
+    {
+        get
+        {
+            return hours;
+        }
+        set
+        {
+            hours = value;
+            price = hours * RATE;
+        }
+    }
+    public double Price
+    {
+        get
+        {
+            return price;
+        }
+    }
+    public override string ToString()
+    {
+        return (GetType() + " " + JobNumber + " " + Customer + " " +
+           Description + " " + Hours + " hours @ " + RATE.ToString("C") +
+           " per hour. Total price is " + Price.ToString("C"));
+    }
+    public override bool Equals(Object e)
+    {
+        bool equal;
+        Job temp = (Job)e;
+        if (JobNumber == temp.JobNumber)
+            equal = true;
+        else
+            equal = false;
+        return equal;
+    }
+    public override int GetHashCode()
+    {
+        return JobNumber;
+    }
+}
---
+++ Week1_Task2/Program.cs
@@ -0,0 +1,113 @@
+﻿/*Step 1: Using the Job class you created in Task 1, write a new application named JobDemo2 that creates an array of five Job objects.
+
+Step 2: Prompt the user for values for each Job . Do not allow duplicate job numbers; force the user to reenter the job when a duplicate job number is entered.
+
+Step 3: When five valid objects have been entered, display them all, plus a total of all prices.
+*/
+
+using System;
+using static System.Console;
+class JobDemo2
+{
+    static void Main()
+    {
+        Job[] jobs = new Job[5];
+        int x, y;
+        double grandTotal = 0;
+        bool goodNum;
+        for (x = 0; x < jobs.Length; ++x)
+        {
+            jobs[x] = new Job(0, "", "", 0);
+            Write("Enter job number ");
+            jobs[x].JobNumber = Convert.ToInt32(ReadLine());
+            goodNum = true;
+            for (y = 0; y < x; ++y)
+            {
+                if (jobs[x].Equals(jobs[y]))
+                    goodNum = false;
+            }
+            while (!goodNum)
+            {
+                Write("Sorry, the job number " +
+                   jobs[x].JobNumber + " is a duplicate. " +
+                   "\nPlease reenter ");
+                jobs[x].JobNumber = Convert.ToInt32(ReadLine());
+                goodNum = true;
+                for (y = 0; y < x; ++y)
+                {
+                    if (jobs[x].Equals(jobs[y]))
+                        goodNum = false;
+                }
+            }
+            Write("Enter customer name ");
+            jobs[x].Customer = ReadLine();
+            Write("Enter job description ");
+            jobs[x].Description = ReadLine();
+            Write("Enter estimated hours for job ");
+            jobs[x].Hours = Convert.ToDouble(ReadLine());
+        }
+        WriteLine("\nSummary:\n");
+        for (x = 0; x < jobs.Length; ++x)
+        {
+            WriteLine(jobs[x].ToString());
+            grandTotal += jobs[x].Price;
+        }
+        WriteLine("\nTotal for all jobs is " + grandTotal.ToString("C"));
+    }
+
+}
+class Job
+{
+    private double hours;
+    private double price;
+    public const double RATE = 45.00;
+    public Job(int num, string cust, string desc, double hrs)
+    {
+        JobNumber = num;
+        Customer = cust;
+        Description = desc;
+        Hours = hrs;
+    }
+    public int JobNumber { get; set; }
+    public string Customer { get; set; }
+    public string Description { get; set; }
+    public double Hours
+    {
+        get
+        {
+            return hours;
+        }
+        set
+        {
+            hours = value;
+            price = hours * RATE;
+        }
+    }
+    public double Price
+    {
+        get
+        {
+            return price;
+        }
+    }
+    public override string ToString()
+    {
+        return (GetType() + " " + JobNumber + " " + Customer + " " +
+           Description + " " + Hours + " hours @" + RATE.ToString("C") +
+           " per hour. Total price is " + Price.ToString("C"));
+    }
+    public override bool Equals(Object e)
+    {
+        bool equal;
+        Job temp = (Job)e;
+        if (JobNumber == temp.JobNumber)
+            equal = true;
+        else
+            equal = false;
+        return equal;
+    }
+    public override int GetHashCode()
+    {
+        return JobNumber;
+    }
+}
