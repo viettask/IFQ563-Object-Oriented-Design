7a9389ad775c2f9d15202304a8a46ecbd9646f37
Viet Nguyen (Henry) <49942312+viettask@users.noreply.github.com>
2024-08-19 23:11:00 +1000

    Add files via upload

---
+++ Week4_Task1/Week4_Task1/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+Step 1: Create a program named WritePatientRecords that allows a doctor's staff to enter data about patients 
+and save the data to a file. 
+Create a Patient class that contains fields for an ID number, name, and current balance owed to the doctor's office.
+
+Step 2: Create a Patient class that contains fields for an ID number, name, and current balance owed to the doctor's office.
+ * */
+
+
+// file saved : C:\Users\vietn\Dropbox\C#\Week4_Task1\Week4_Task1\bin\Debug\net8.0\Patients.txt
+using System;
+using static System.Console;
+using System.IO;
+class WritePatientRecords
+{
+    static void Main()
+    {
+        const char DELIM = ',';
+        const string END = "999";
+        const string FILENAME = "Patients.txt";
+        Patient patient = new Patient();
+        FileStream outFile = new FileStream(FILENAME, FileMode.Create, FileAccess.Write);
+        StreamWriter writer = new StreamWriter(outFile);
+        Write("Enter patient ID number or " + END +
+           " to quit >> ");
+        patient.IdNum = ReadLine();
+        while (patient.IdNum != END)
+        {
+            Write("Enter last name >> ");
+            patient.Name = ReadLine();
+            Write("Enter balance >> ");
+            patient.Balance = Convert.ToDouble(ReadLine());
+            writer.WriteLine(patient.IdNum + DELIM + patient.Name +
+               DELIM + patient.Balance);
+            Write("Enter next patient ID number or " +
+               END + " to quit >> ");
+            patient.IdNum = ReadLine();
+        }
+        writer.Close();
+        outFile.Close();
+    }
+}
+
+class Patient
+{
+    public string IdNum { get; set; }
+    public string Name { get; set; }
+    public double Balance { get; set; }
+    public new string ToString()
+    {
+        return ("#" + IdNum + ',' + Name + ',' + Balance);
+    }
+}
---
+++ Week4_Task2/Week4_Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Step 1: Create a program named ReadPatientRecords​ 
+ * that reads the file created in Task 1 and displays each patient's data on the screen.
+ */
+using System;
+using static System.Console;
+using System.IO;
+class ReadPatientRecords
+{
+    static void Main()
+    {
+        const char DELIM = ',';
+        const string FILENAME = "Patients.txt";
+        Patient patient = new Patient();
+        FileStream inFile = new FileStream(FILENAME, FileMode.Open, FileAccess.Read);
+        StreamReader reader = new StreamReader(inFile);
+        string recordIn;
+        string[] fields;
+        WriteLine("\n{0,-10}{1,-18}{2,10}\n", "IdNumber", "Name", "Balance");
+        recordIn = reader.ReadLine();
+        while (recordIn != null)
+        {
+            fields = recordIn.Split(DELIM);
+            patient.IdNum = fields[0];
+            patient.Name = fields[1];
+            patient.Balance = Convert.ToDouble(fields[2]);
+            WriteLine("{0,-10}{1,-18}{2, 10}", patient.IdNum, patient.Name, patient.Balance.ToString("C"));
+            recordIn = reader.ReadLine();
+        }
+
+        reader.Close();
+        inFile.Close();
+    }
+}
+
+class Patient
+{
+    public string IdNum { get; set; }
+    public string Name { get; set; }
+    public double Balance { get; set; }
+    public new string ToString()
+    {
+        return ("#" + IdNum + ',' + Name + ',' + Balance);
+    }
+}
